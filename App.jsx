35305fd58008d355a75dd0746ba6aaf3cef39e7c
Michael Vasilevsky <michael@dikuw.com>
2024-12-14 03:12:54 -0500

    update Primary and Secondary colors

--- App.jsx
+++ App.jsx
@@ -39,8 +39,8 @@ const theme = {
     warning: '#FF9800',
     info: '#2196F3',
     white: '#FFFFFF',
-    primary: '#000000',
-    secondary: '#000000',
+    primary: '#ffcb76',
+    secondary: '#ffbf55',
   },
   spacing: {
     small: 8,
