73bc38908d4e6c2e375cd5484019618edbcf1b2e
Michael Vasilevsky <michael@dikuw.com>
2024-12-22 08:22:08 -0500

    add translation to name

--- src/components/header/TopBanner.jsx
+++ src/components/header/TopBanner.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Picker } from '@react-native-picker/picker';
 import { useTranslation } from "react-i18next";
 import styled from 'styled-components/native';
 
@@ -14,52 +13,14 @@ const BannerText = styled.Text`
   text-align: center;
 `;
 
-const SelectContainer = styled.View`
-  position: absolute;
-  right: 4px;
-  top: 8px;
-  width: 120px;
-  height: 30px;
-  ${Platform.select({
-    ios: `
-      background-color: transparent;
-    `,
-    android: `
-      background-color: ${props => props.theme.colors.primary};
-    `
-  })}
-`;
-
-// const StyledPicker = styled(Picker)`
-//   width: 100%;
-//   height: 100%;
-//   color: ${props => props.theme.colors.white};
-// `;
-
 export default function TopBanner(props) {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
-  const handleChange = (value) => {
-    i18n.changeLanguage(value);
-  };
-  
   return (
     <BannerContainer>
       <BannerText>
-        {t("Welcome")} {props.name}!
+        {t("Welcome")} {t(props.name)}!
       </BannerText>
-      {/* Remove this section:
-      <SelectContainer>
-        <StyledPicker
-          selectedValue={i18n.language}
-          onValueChange={handleChange}
-          mode="dropdown"
-        >
-          <StyledPicker.Item label={t("Spanish")} value="es" />
-          <StyledPicker.Item label={t("English")} value="en-US" />
-        </StyledPicker>
-      </SelectContainer>
-      */}
     </BannerContainer>
   );
 }
